16e1bad781a212313c27b9ab2a0baeca159a143d
qing-rain <353752459@qq.com>
2021-11-11 14:28:52 +0800

    第15期-本地化与全球化的多语言实现

---
+++ QingStack/Services/DeviceCenter/QingStack.DeviceCenter.API/Controllers/ResourcesController.cs
@@ -0,0 +1,36 @@
+﻿/*----------------------------------------------------------------
+    Copyright (C) 2021 QingRain
+
+    文件名：ResourcesController.cs
+    文件功能描述：资源文件控制器
+
+
+    创建标识：QingRain - 20211111
+
+ ----------------------------------------------------------------*/
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+namespace QingStack.DeviceCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourcesController : ControllerBase
+    {
+        private readonly IStringLocalizerFactory _localizerFactory;
+
+        public ResourcesController(IStringLocalizerFactory localizerFactory)
+        {
+            _localizerFactory = localizerFactory;
+        }
+
+        [HttpGet]
+        public string GetHelloWorld()
+        {
+            IStringLocalizer stringLocalizer = _localizerFactory.Create("Welcome", Assembly.GetExecutingAssembly().ToString());
+
+            return stringLocalizer["HelloWorld"].Value;
+        }
+    }
+}
--- QingStack/Services/DeviceCenter/QingStack.DeviceCenter.API/Startup.cs
+++ QingStack/Services/DeviceCenter/QingStack.DeviceCenter.API/Startup.cs
@@ -1,3 +1,16 @@
+/*----------------------------------------------------------------
+    Copyright (C) 2021 QingRain
+
+    文件名：Startup.cs
+    文件功能描述：启动项
+
+
+    创建标识：QingRain - 
+
+    修改标识：QingRain - 20211111
+    修改描述：注入本地化资源、多语言设置
+
+ ----------------------------------------------------------------*/
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +24,7 @@ using QingStack.DeviceCenter.Domain;
 using QingStack.DeviceCenter.Domain.Repositories;
 using QingStack.DeviceCenter.Infrastructure;
 using System;
+using System.Linq;
 
 namespace QingStack.DeviceCenter.API
 {
@@ -26,10 +40,12 @@ namespace QingStack.DeviceCenter.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //本地化资源
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
             services.AddDomainLayer();
             services.AddInfrastructureLayer(Configuration).AddApplicationLayer();
             services.AddTenantMiddleware();
-            services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+            services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies())).AddDataAnnotationsLocalization();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "QingStack.DeviceCenter.API", Version = "v1" });
@@ -40,6 +56,17 @@ namespace QingStack.DeviceCenter.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //多语言中间件
+            string[] supportedCultures = new[] { "zh-CN", "en-US" };
+            RequestLocalizationOptions localizationOptions = new()
+            {
+                ApplyCurrentCultureToResponseHeaders = false
+            };
+            //设置默认语言文化
+            localizationOptions.SetDefaultCulture(supportedCultures.First()).AddSupportedCultures(supportedCultures).AddSupportedUICultures(supportedCultures);
+            //拦截请求解析语言版本
+            app.UseRequestLocalization(localizationOptions);
+
             app.UseTenantMiddleware();
 
             using (IServiceScope serviceScope = app.ApplicationServices.CreateScope())
